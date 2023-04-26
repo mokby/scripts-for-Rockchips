bd709c0c1b40ee4e067c7e1085d75449955998a3
Rybyen King <2750889421@qq.com>
2023-04-26 13:37:29 +0800

    Add files via upload

--- query_NPU_occupy.sh
+++ query_NPU_occupy.sh
@@ -1,6 +1,6 @@
 #@https://github.com/mokby/scripts-for-Rockchips
 #please contact me with Rubyen233@qq.com
-LOG_FILE="/home/topeet/develop/test/log.txt"
+LOG_FILE="./NPU_log.txt"
 > $LOG_FILE
 trap "echo 'Exiting...'; exit 0" SIGINT
 while true; do
